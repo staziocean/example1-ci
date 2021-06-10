441f42aea1f4e50ed758b13ba60fe74fe3938f0a
Salim Tazi <stazi@iocean.fr>
2021-06-10 16:11:11 +0200

    Ta soeur

--- src/main/java/fr/diginamic/ci/Main.java
+++ src/main/java/fr/diginamic/ci/Main.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    private final static Logger LOG = LoggerFactory.getLogger(Main.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         LOG.info("Hello world!");
