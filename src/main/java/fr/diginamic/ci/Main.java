d9005bc9aa34b24b88ec622fc88339169eb4caca
Salim Tazi <stazi@iocean.fr>
2021-06-10 16:06:25 +0200

    Fix sonar issue

--- src/main/java/fr/diginamic/ci/Main.java
+++ src/main/java/fr/diginamic/ci/Main.java
@@ -1,7 +1,13 @@
 package fr.diginamic.ci;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private final static Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LOG.info("Hello world!");
     }
 }
